47c73f120949d693fd812c93bb08d8532de99d35
Vorun5 <firdavsinurov326@gmail.com>
2021-11-16 14:53:23 +0300

    before redesign

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -14,49 +14,80 @@ export enum Figures {
 }
 
 export enum Colors {
-    Red = 'rgb(255, 0, 0)', //#FF0000
-    Blue = 'rgb(0, 0, 255)', //#0000FF
-    Green = 'rgb(0, 255, 0)', //#00FF00
-    Yellow = 'rgb(255, 255, 0)', //#FFFF00
-    Purple = 'rgb(128, 0, 128)', //#800080
-    Aqua = 'rgb(0, 255, 255)', //#00FFFF
-    Fuchsia = 'rgb(255, 0, 255)', //#FF00FF
-    Pink = 'rgb(255, 192, 203)', //#FFC0CB
-    Grey = 'rgb(128, 128, 128)' //#808080
+    Red = 'rgb(255, 77, 24)', //#f24d18
+    Blue = 'rgb(105, 156, 247)', //#699cf7
+    Green = 'rgb(6, 171, 87)', //#06ab57
+    Yellow = 'rgb(255, 200, 0)', //#ffc800
+    Purple = 'rgb(152, 69, 255)', //#9845ff
+    Grey = 'rgb(128, 128, 128)', //#808080
+    None = 'rgb(255, 255, 255)',
 }
+
 function App() {
+    const allColors = [Colors.None, Colors.Red, Colors.Blue, Colors.Green, Colors.Yellow, Colors.Purple, Colors.Grey];
     const [filterToolActive, setFilterToolActive] = useState(false);
     const [artToolActive, setArtToolActive] = useState(false);
+    const [filter, setFilter] = useState(Colors.None);
+
+    const [w, setW] = useState(800);
+    const [h, setH] = useState(600);
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+    const styleCard = {
+        width: w,
+        height: h,
+        top: x,
+        left: y,
+    }
+    const onClick = (e: any) => {
+        const parent = e.target.parentNode.getBoundingClientRect();
+        const element = e.target.getBoundingClientRect();
+
+        const x = element.top - parent.top;
+        const y = element.left - parent.left;
+
+        console.log(x, y);
+    };
     return (
         <div className="App" onClick={() => {
             setFilterToolActive(false);
             setArtToolActive(false);
         }}>
-            <AppBackground>
-                <div className="tools">
-                    <div onClick={e => {
-                        e.stopPropagation();
-                        setArtToolActive(false);
-                    }}>
-                        <FilterTool active={filterToolActive} setActive={setFilterToolActive}/>
-                    </div>
-                    <div onClick={e => {
-                        e.stopPropagation();
-                        setFilterToolActive(false);
-                    }}>
-                        <ArtTool active={artToolActive} setActive={setArtToolActive}/>
-                    </div>
-                    <TextTool/>
-                    <IMGTool/>
-                    <TemplateTool/>
-                    <div className="tools__container" >
-                        <HistoryTool/>
-                        <SaveTool/>
-                    </div>
+            <div className="container">
+            <div className="header"></div>
+            <div className="work-space">
+
+                <div id="card" style={styleCard} onClick={onClick} className="card"></div>
+
+            </div>
+            <div className="tools">
+                <div onClick={e => {
+                    e.stopPropagation();
+                    setArtToolActive(false);
+                }}>
+                    <FilterTool
+                        color={filter}
+                        setColor={setFilter}
+                        colorList={allColors}
+                        active={filterToolActive} setActive={setFilterToolActive}/>
                 </div>
-            </AppBackground>
+                <div onClick={e => {
+                    e.stopPropagation();
+                    setFilterToolActive(false);
+                }}>
+                    <ArtTool active={artToolActive} setActive={setArtToolActive}/>
+                </div>
+                <TextTool/>
+                <IMGTool/>
+                <TemplateTool/>
+                <div className="tools__container">
+                    <HistoryTool/>
+                    <SaveTool/>
+                </div>
+            </div>
+            </div>
         </div>
-);
+    );
 }
 
 export default App;
--- src/components/FilterTool/FilterTool.tsx
+++ src/components/FilterTool/FilterTool.tsx
@@ -1,33 +1,31 @@
 import React, {useState} from 'react';
 import c from './FilterTool.module.scss'
 import {Colors} from "../App/App";
+import ColorSelect from "../ColorSelect/ColorSelect";
 
 interface FilterToolProps {
     active: boolean;
     setActive: React.Dispatch<React.SetStateAction<boolean>>;
+    color: Colors,
+    setColor:  React.Dispatch<React.SetStateAction<Colors>>,
+    colorList: Colors[],
 }
 
-const FilterTool: React.FC<FilterToolProps> = ({active, setActive}) => {
-    const [s, setS] = useState(Colors.Pink);
-    const colorView = {
-        backgroundColor: s,
-    }
-    const allColors = [1, 2, 3, 4];
+const FilterTool: React.FC<FilterToolProps> = ({active, setActive, color, setColor, colorList}) => {
     return (
         <div className="tools__filter tools__item">
 
-            <div className={c.body}>
+            <div className={c.container}>
 
                 <div className={active ? c.face + ' ' + c.face_active : c.face} onClick={() => setActive(!active)}>
-
                     <div className={c.face__color}>
-                        <div className={c.face__color_view} style={colorView}/>
+                        <div className={c.face__color_view} style={{backgroundColor: color}}/>
                     </div>
                     <div className={c.face__icon}/>
                 </div>
 
                 <div className={active ? c.select + ' ' + c.select_active : c.select}>
-
+                    <ColorSelect color={color} setColor={setColor} colorList={colorList}/>
                 </div>
             </div>
 
