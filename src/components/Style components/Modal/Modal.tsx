9f94690a31813413c65caff28ec3d44965bd7d4e
Vorun5 <firdavsinurov326@gmail.com>
2022-01-09 16:31:36 +0300

    create new file

---
+++ src/components/Style components/Modal/Modal.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import c from "./Modal.module.scss"
+
+interface ModalProps {
+    children?: React.ReactNode,
+}
+
+const Modal: React.FC<ModalProps> = ({children}) => {
+    return (<div className={c.modal}>
+        <div className={c.modal_content}>{children}</div>
+    </div>)
+};
+
+export default Modal;
